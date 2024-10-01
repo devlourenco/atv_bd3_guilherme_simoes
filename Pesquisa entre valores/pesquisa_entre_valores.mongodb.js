db8609fc1c80ecfcbec7b815d628e6cf1ca0e075
devlourenco <guilherme.gsldev@gmail.com>
2024-09-30 22:18:13 -0300

    Pesquisa entre valores e de categorias

---
+++ Pesquisa entre valores/pesquisa_entre_valores.mongodb.js
@@ -0,0 +1,9 @@
+const database = "bd3_atv2";
+
+const collection = "bd3_atv2_produtos";
+
+use(database);
+
+db[collection].find({
+  valor: { $gte: 150.9, $lte: 200.9 },
+});
--- teste.mongodb.js
+++ teste.mongodb.js
@@ -5,4 +5,5 @@ const collection = 'bd3_atv2_produtos'
 
 use(database)
 
+
 db[collection].find({})
